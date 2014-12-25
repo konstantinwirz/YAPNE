8e1755a8f932368f37395978a453df2f6643ff78
Konstantin Wirz <konstantin.wirz@gmail.com>
2014-12-25 16:48:54 +0100

    handle mode change with single method

--- src/main/java/de/kwirz/yapne/app/AppController.java
+++ src/main/java/de/kwirz/yapne/app/AppController.java
@@ -1,17 +1,19 @@
 package de.kwirz.yapne.app;
 
 import de.kwirz.yapne.model.PetriNet;
-import de.kwirz.yapne.presentation.PetriNetArcPresentation;
 import de.kwirz.yapne.presentation.PetriNetElementPresentation;
 import de.kwirz.yapne.presentation.PetriNetNodePresentation;
 import de.kwirz.yapne.presentation.PetriNetPresentation;
 import de.kwirz.yapne.utils.Settings;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Point2D;
-import javafx.scene.Node;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
@@ -20,11 +22,13 @@ import javafx.stage.FileChooserBuilder;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import de.kwirz.yapne.io.PnmlParser;
@@ -122,56 +126,127 @@ public class AppController implements Initializable {
 
     @FXML
     public void saveDocument() {
+        PetriNet model = canvas.getModel();
+
+        assert model != null;
+
+        File file = null;
+
+        if (!currentFileName.isEmpty()) {
+            file = new File(currentFileName);
+        } else {
+            final String initialDirectory =
+                    Settings.getInstance().getValue("initial_directory", System.getProperty("user.home"));
+            final FileChooser fileChooser = FileChooserBuilder.create()
+                    .title("Save PNML file")
+                    .initialDirectory(new File(initialDirectory))
+                    .build();
+            file = fileChooser.showSaveDialog(primaryStage);
+
+            if (file == null) {
+                logger.log(Level.WARNING, "no file to save choosen");
+                return;
+            }
+        }
+
+        assert file != null;
+
+        try {
+            if (!file.toPath().getFileName().endsWith(".pnml")) {
+                file = new File(file.toPath().toString() + ".pnml");
+            }
+
+            if (!file.exists())
+                file.createNewFile();
+
+            FileWriter writer = new FileWriter(file);
+            writer.write(canvas.getModel().toXml());
+            writer.close();
+
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
 
     }
-    
-    @FXML
-    public void activateEditingMode() {
-    	onModeChanged(AppMode.EDITING);
-    }
-    
-    @FXML
-    public void activatePlaceCreationMode() {
-    	onModeChanged(AppMode.PLACE_CREATION);
-    }
-    
-    @FXML
-    public void activateTransitionCreationMode() {
-        onModeChanged(AppMode.TRANSITION_CREATION);
-    }
-    
-    @FXML
-    public void activateArcCreationMode() {
-    	onModeChanged(AppMode.ARC_CREATION);
-    }
 
-    private void onModeChanged(AppMode mode) {
-        this.mode = mode;
+    @FXML
+    public void handleModeChange(ActionEvent event) {
+        assert event.getEventType() == ActionEvent.ACTION;
+
+        ToggleButton sourceButton = ((ToggleButton) event.getSource());
+
+        if (!sourceButton.isSelected()) {
+            mode = AppMode.EDITING;
+        } else {
+            switch(sourceButton.getId()) {
+                case "place_mode_tb" : mode = AppMode.PLACE_CREATION; break;
+                case "transition_mode_tb" : mode = AppMode.TRANSITION_CREATION; break;
+                case "arc_mode_tb" : mode = AppMode.ARC_CREATION; break;
+                default: throw new IllegalArgumentException("this place will be never reached");
+            }
+        }
+
+        logger.log(Level.INFO, "new mode: " + mode);
     }
 
     @FXML
     private void handleMouseEvent(MouseEvent event) {
+        final Object source = event.getSource();
+        final Object target = event.getTarget();
+        final EventType<? extends Event> eventType = event.getEventType();
+
         switch (mode) {
+
             case EDITING:
-                if (event.getEventType().equals(MouseEvent.MOUSE_DRAGGED)) {
-                    Object node = event.getSource();
-                    if (node instanceof PetriNetNodePresentation) {
-                        canvas.moveNode((PetriNetNodePresentation) node,
-                                new Point2D(event.getSceneX(), event.getSceneY()));
-                    }
-                } else if (event.getEventType().equals(MouseEvent.MOUSE_CLICKED)) {
-                    Object node = event.getSource();
-                    if (node instanceof PetriNetElementPresentation)
-                        canvas.selectElement((PetriNetElementPresentation) node);
+                if ( eventType == MouseEvent.MOUSE_DRAGGED && source instanceof PetriNetNodePresentation ) {
+                    canvas.selectElement((PetriNetNodePresentation) source);
+                    canvas.moveNode((PetriNetNodePresentation) source,
+                            new Point2D(event.getSceneX(), event.getSceneY()));
+                } else if ( eventType == MouseEvent.MOUSE_CLICKED && source instanceof PetriNetElementPresentation) {
+                    System.out.println(event);
+                    canvas.selectElement((PetriNetElementPresentation) source);
+                } else if ( eventType == MouseEvent.MOUSE_CLICKED && target instanceof  PetriNetPresentation ) {
+                    canvas.unselectElement(canvas.getSelectedElement());
                 }
                 break;
+
             case PLACE_CREATION:
-                if (event.getEventType().equals(MouseEvent.MOUSE_CLICKED))
+                if (    eventType == MouseEvent.MOUSE_CLICKED &&
+                        source instanceof PetriNetPresentation &&
+                        target instanceof PetriNetPresentation ) {
                     canvas.createPlace(event.getSceneX(), event.getSceneY());
+                }
                 break;
+
             case TRANSITION_CREATION:
-                if (event.getEventType().equals(MouseEvent.MOUSE_CLICKED))
+                if (    eventType == MouseEvent.MOUSE_CLICKED &&
+                        source instanceof PetriNetPresentation &&
+                        target instanceof PetriNetPresentation ) {
                     canvas.createTransition(event.getSceneX(), event.getSceneY());
+                }
+                break;
+
+            case ARC_CREATION:
+                if ( eventType == MouseEvent.MOUSE_CLICKED ) {
+                    if ( canvas.getSelectedElement() == null) {
+                        if ( source instanceof PetriNetNodePresentation )
+                            canvas.selectElement((PetriNetElementPresentation) source);
+
+                    } else { // element is selected
+                        if ( source instanceof PetriNetPresentation &&
+                             target instanceof PetriNetPresentation) {
+                            canvas.unselectElement(canvas.getSelectedElement());
+                        } else if ( source instanceof PetriNetNodePresentation &&
+                                !canvas.getSelectedElement().getClass().equals(source.getClass()) ) {
+                            canvas.createArc( (PetriNetNodePresentation) canvas.getSelectedElement(),
+                                    (PetriNetNodePresentation) source);
+                            canvas.unselectElement(canvas.getSelectedElement());
+                        }
+                    }
+                }
+
+                break;
+
             default:
         }
     }
